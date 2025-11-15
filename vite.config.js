2ff5ba5d55c6e291cf897df8914cbea96b8c9163
ZACKSHANE <zackyalhafizh1903@gmail.com>
2025-11-15 17:58:16 +0700

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // <- tambahkan ini
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/EcoCraft/",  // <-- WAJIB
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
